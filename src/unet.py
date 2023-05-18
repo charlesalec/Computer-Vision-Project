3f13f209aa9d01f797382c013073818a11d6fc4c
charlesalec <42868423+charlesalec@users.noreply.github.com>
2023-05-18 16:27:14 +0200

    fixed unet, model now trains.

--- src/unet.py
+++ src/unet.py
@@ -7,12 +7,10 @@ import os
 import glob
 import matplotlib.pyplot as plt
 import cv2
-import rawpy
 # import Dataset calss
 from torch.utils.data import Dataset
 
 
-# From: https://github.com/nikhilroxtomar/Semantic-Segmentation-Architecture/blob/main/PyTorch/unet.py
 
 """ Convolutional block:
     It follows a two 3x3 convolutional layer, each followed by a batch normalization and a relu activation.
@@ -40,16 +38,13 @@ class conv_block(nn.Module):
 
         return x
 
-""" Encoder block:
-    It consists of an conv_block followed by a max pooling.
-    Here the number of filters doubles and the height and width half after every block.
-"""
+
 class encoder_block(nn.Module):
     def __init__(self, in_c, out_c):
         super().__init__()
 
         self.conv = conv_block(in_c, out_c)
-        self.pool = nn.MaxPool2d((2, 2))
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
     def forward(self, inputs):
         x = self.conv(inputs)
@@ -57,66 +52,64 @@ class encoder_block(nn.Module):
 
         return x, p
 
-""" Decoder block:
-    The decoder block begins with a transpose convolution, followed by a concatenation with the skip
-    connection from the encoder block. Next comes the conv_block.
-    Here the number filters decreases by half and the height and width doubles.
-"""
+
 class decoder_block(nn.Module):
     def __init__(self, in_c, out_c):
         super().__init__()
 
-        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
-        self.conv = conv_block(out_c+out_c, out_c)
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2)
+        self.conv = conv_block(in_c, out_c)
 
     def forward(self, inputs, skip):
         x = self.up(inputs)
-        x = torch.cat([x, skip], axis=1)
+        x = torch.cat([x, skip], dim=1)
         x = self.conv(x)
 
         return x
 
 
-class build_unet(nn.Module):
-    def __init__(self):
-        super().__init__()
+class UNet(nn.Module):
+    def __init__(self, in_c, out_c, num_filters=64):
+        super(UNet, self).__init__()
 
-        """ Encoder """
-        self.e1 = encoder_block(1, 4)
-        self.e2 = encoder_block(4, 64)
-        self.e3 = encoder_block(64, 128)
-        self.e4 = encoder_block(128, 256)
+        # Encoder
+        self.encoder1 = encoder_block(in_c, num_filters)
+        self.encoder2 = encoder_block(num_filters, num_filters * 2)
+        self.encoder3 = encoder_block(num_filters * 2, num_filters * 4)
+        self.encoder4 = encoder_block(num_filters * 4, num_filters * 8)
 
-        """ Bottleneck """
-        self.b = conv_block(256, 512)
+        # Bottleneck
+        self.bottleneck = conv_block(num_filters * 8, num_filters * 16)
 
-        """ Decoder """
-        self.d1 = decoder_block(512, 256)
-        self.d2 = decoder_block(256, 128)
-        self.d3 = decoder_block(128, 64)
-        self.d4 = decoder_block(64, 4)
+        # Decoder
+        self.decoder1 = decoder_block(num_filters * 16, num_filters * 8)
+        self.decoder2 = decoder_block(num_filters * 8, num_filters * 4)
+        self.decoder3 = decoder_block(num_filters * 4, num_filters * 2)
+        self.decoder4 = decoder_block(num_filters * 2, num_filters)
 
-        """ Classifier """
-        self.outputs = nn.Conv2d(4, 4, kernel_size=1, padding=0)
+        # Classifier
+        self.outputs = nn.Conv2d(num_filters, out_c, kernel_size=1)
 
-    def forward(self, inputs):
-        """ Encoder """
-        s1, p1 = self.e1(inputs)
-        s2, p2 = self.e2(p1)
-        s3, p3 = self.e3(p2)
-        s4, p4 = self.e4(p3)
+    def forward(self, x):
+        # Encoder
+        enc1, x = self.encoder1(x)
+        enc2, x = self.encoder2(x)
+        enc3, x = self.encoder3(x)
+        enc4, x = self.encoder4(x)
 
-        """ Bottleneck """
-        b = self.b(p4)
+        # Bottleneck
+        x = self.bottleneck(x)
 
-        """ Decoder """
-        d1 = self.d1(b, s4)
-        d2 = self.d2(d1, s3)
-        d3 = self.d3(d2, s2)
-        d4 = self.d4(d3, s1)
+        # Decoder
+        x = self.decoder1(x, enc4)
+        x = self.decoder2(x, enc3)
+        x = self.decoder3(x, enc2)
+        x = self.decoder4(x, enc1)
 
-        """ Classifier """
-        outputs = self.outputs(d4)
-        outputs = nn.functional.pixel_shuffle(outputs, 2)
+        # Classifier
+        outputs = self.outputs(x)
 
         return outputs
+    
+
+# Inspired by: https://github.com/nikhilroxtomar/Semantic-Segmentation-Architecture/blob/main/PyTorch/unet.py
